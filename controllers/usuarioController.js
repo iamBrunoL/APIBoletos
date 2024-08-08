fb38ed20bbc80686cb084cab0068b55d3cabb5e8
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-08-08 17:49:01 -0600

    segunda comprobacion del tipo de usuario incluido en el  token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -242,16 +242,15 @@ exports.loginUsuario = async (req, res) => {
 
         // Mostrar el payload en consola para verificación
         console.log("Payload generado:", payload);
-
+        
         const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        // Registrar inicio de sesión exitoso con ID de usuario
-        await registrarLog('loginUsuario', req, { message: 'Inicio de sesión exitoso', usuario: usuario.idUsuario }, 'info');
 
         // Decodificar el token para mostrar el tipo de usuario
         const decoded = jwt.decode(token);
         console.log("Tipo de usuario después de decodificación:", decoded.tipo);
 
+        // Registrar inicio de sesión exitoso con ID de usuario
+        await registrarLog('loginUsuario', req, { message: 'Inicio de sesión exitoso', usuario: usuario.idUsuario }, 'info');
 
         res.json({ token });
     } catch (error) {
