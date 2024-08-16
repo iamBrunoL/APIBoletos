4517fb98d7541e642cbb6478434fdda3b0ad029e
iamBrunoL <141052273+iamBrunoL@users.noreply.github.com>
2024-08-15 22:05:18 -0600

    ajustes de ruta  de cartelera por dias

--- routes/carteleraRoutes.js
+++ routes/carteleraRoutes.js
@@ -7,7 +7,7 @@ const checkRole = require('../middleware/roleMiddleware');
 // Crear una nueva entrada en la cartelera
 router.post('/', verifyToken, checkRole(['admin']), carteleraController.createCartelera);
 
-router.get('/', verifyToken, checkRole(['admin','cliente']), carteleraController.getCarteleraPorDia);
+router.get('/carteleraDia', verifyToken, checkRole(['admin','cliente']), carteleraController.getCarteleraPorDia);
 
 // Obtener todas las entradas en la cartelera
 router.get('/', verifyToken, checkRole(['admin']), carteleraController.getAllCarteleras);
